325d405894c5432be637392c91e67549a2c6649e
Liquid Monks <info@liquidmonks.com>
2022-12-01 17:32:48 -0600

    script.js includes a function code block with conditional loop statements loops throug the api drinks array and returns a drink name, image, and instructions. The script also expands the number of drinks the search query returns to a value of x (dev defines the value of x at the script level and is not accessible to the user).

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -40,9 +40,73 @@ function mainSearch(event) {
     });
 }
 
-/**************** TODO: This code block returns four drinks from the array of drinks ****************/
+/**************** This code block returns drinks + image + ingredients + instructions ****************/
 
-// TODO: Create a function that returns drink name, category, image, and instructions
+/*
+// function to search by name and by ingredient
+function mainSearch(event) {
+  event.preventDefault();
+  drinkList.innerHTML = "";
+  let queryParam = dropdownChoice.value === "cocktail" ? `search.php?s=${searchInput.value}` : `filter.php?i=${searchInput.value}`;
+  let requestUrl = `${baseUrl}${queryParam}`;
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.drinks);
+      let indexes = [];
+      //   RANDOM INFO
+      //   This function returns x number of drinks from an array of drinks
+      let randomDrink;
+      let dIndex;
+      let length = data.drinks.length;
+       for (let j = 0; j < length; j++) {
+            randomDrink = Math.floor(Math.random() * length);
+            if (!indexes.includes(randomDrink)) {
+                indexes.push(randomDrink);
+            }
+            if (indexes.length >=4) {   // 4 is the number of drinks you want to return, you can change this number
+                break;
+            }
+        }
+      // This loop returns a random drink from an array of drinks along with the drink's ingredients, image, and instructions
+        for (let i = 0; i < indexes.length; i++) {
+          let ingredients = [];
+          dIndex = indexes[i];
+          let ingredientsData,instructionsData;
+            for (let k = 1; k <= 15; k++) {
+              if (`strIngredient${k}` in data.drinks[dIndex] && data.drinks[dIndex][`strIngredient${k}`] != null) {
+                ingredients.push(data.drinks[dIndex][`strIngredient${k}`]);
+              }else{
+                break;
+              }
+            }
+            // console.log(ingredients);
+            if (ingredients.length != 0) {
+              ingredientsData = `<p>Ingredients: ${ingredients.join(", ")}</p>`;
+            }else{
+              ingredientsData = '';
+            }
+            if (data.drinks[dIndex].strInstructions !== undefined) {
+              instructionsData = `<p>Instructions: ${data.drinks[dIndex].strInstructions}</p>`;
+            } else {
+              instructionsData = '';
+            }
+            drinkList.innerHTML += `<div>
+            <h4 style="font-size: 22px;">${data.drinks[dIndex].strDrink}</h4>
+            <p><img height="200" width="200" src="${data.drinks[dIndex].strDrinkThumb}"></p>
+            ${ingredientsData}
+            ${instructionsData}
+            </div>`;
+        }
+    
+      
+    });
+}
+
+*/
 
 // EVENT LISTENERS
 // init
