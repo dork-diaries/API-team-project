5b5195a4b278b0875acf52736cdf2cd1703883a8
Liquid Monks <info@liquidmonks.com>
2022-11-30 19:52:45 -0600

    script.js includes a declaration code block that randomly selects a drink from an arrayof the drink array and prints it to index.html. Additionally, it includes a pseudo-code comment with an associating code-block that returns all possible drinks.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,7 +6,9 @@ let drinkList = document.querySelector("#drink-list");
 let baseUrl = "https://www.thecocktaildb.com/api/json/v1/1/";
 
 /*********************************** Search by Name ***********************************/
-// FUNCTIONS
+
+// function to search by name and by ingredient
+
 function mainSearch(event) {
   event.preventDefault();
   drinkList.innerHTML = "";
@@ -19,14 +21,26 @@ function mainSearch(event) {
     })
     .then(function (data) {
       console.log(data);
-    //   RANDOM INFO
-    //   let length = data.drinks.length;
-    //   let random = math.floor(math.random());
-      data.drinks.slice(0, 10).forEach((drink) => {
+
+      //   This declaration declares a block-scoped local variable that randomly selects a drink from the array of drinks
+      let length = data.drinks.length;
+      let randomDrink = Math.floor(Math.random() * length);
+
+      /**************** This code block returns all drinks from the array of drinks ****************/
+
+      /* data.drinks.slice(randomDrink).forEach((drink) => {
         drinkList.innerHTML += `<div>${drink.strDrink}</div>`;
       });
     });
 }
+*/
+
+      /**************** This code block returns only one drink from the array of drinks ****************/
+      drinkList.innerHTML += `<div>${data.drinks[randomDrink].strDrink}</div>`;
+    });
+}
+
+/**************** This code block returns four drinks from the array of drinks ****************/
 
 // EVENT LISTENERS
 // init
