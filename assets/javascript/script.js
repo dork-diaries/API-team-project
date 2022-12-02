1eb9c3e63b67ec4a7ef3ed379125652be147752d
Liquid Monks <info@liquidmonks.com>
2022-12-01 22:50:56 -0600

    script.js files resolves the issues with the ingredients query not displaying ingredients list and instructions. Additionally, it utilizes a secondary api string from lookp.php which introduces strMeasure to the code and concatenates the ingredient measurements with ingredients, then outputs it to the index.html.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,25 +4,24 @@ let dropdownChoice = document.querySelector("#dropdown-choice");
 let searchInput = document.querySelector("#search-input");
 let drinkList = document.querySelector("#drink-list");
 let baseUrl = "https://www.thecocktaildb.com/api/json/v1/1/";
-
 /**************** This code block returns drinks + image + ingredients + instructions ****************/
-
-// function to search by name and by ingredient
+// FUNCTIONS
 function mainSearch(event) {
   event.preventDefault();
   drinkList.innerHTML = "";
   let queryParam = dropdownChoice.value === "cocktail" ? `search.php?s=${searchInput.value}` : `filter.php?i=${searchInput.value}`;
   let requestUrl = `${baseUrl}${queryParam}`;
-
+  console.log(requestUrl);
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
+      let idUrl = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
       console.log(data.drinks);
       let indexes = [];
       //   RANDOM INFO
-      //   This function returns x number of drinks from an array of drinks
+      //   This function returns a random drink from an array of drinks
       let randomDrink;
       let dIndex;
       let length = data.drinks.length;
@@ -31,44 +30,77 @@ function mainSearch(event) {
         if (!indexes.includes(randomDrink)) {
           indexes.push(randomDrink);
         }
-        if (indexes.length >= 100) {
-          // 4 is the number of drinks you want to return, you can change this number
+        if (indexes.length >= 4) {
           break;
         }
       }
-      // This loop returns a random drink from an array of drinks along with the drink's ingredients, image, and instructions
+      let cocktail, d_id;
+      // TODO: Restrict the number of drinks displayed to x, using hide class
       for (let i = 0; i < indexes.length; i++) {
-        let ingredients = [];
         dIndex = indexes[i];
-        let ingredientsData, instructionsData;
-        for (let k = 1; k <= 15; k++) {
-          if (`strIngredient${k}` in data.drinks[dIndex] && data.drinks[dIndex][`strIngredient${k}`] != null) {
-            ingredients.push(data.drinks[dIndex][`strIngredient${k}`]);
-          } else {
-            break;
-          }
-        }
-        // console.log(ingredients);
-        if (ingredients.length != 0) {
-          ingredientsData = `<p>Ingredients: ${ingredients.join(", ")}</p>`;
-        } else {
-          ingredientsData = "";
-        }
-        if (data.drinks[dIndex].strInstructions !== undefined) {
-          instructionsData = `<p>Instructions: ${data.drinks[dIndex].strInstructions}</p>`;
-        } else {
-          instructionsData = "";
-        }
-        drinkList.innerHTML += `<div>
-            <h4 style="font-size: 22px;">${data.drinks[dIndex].strDrink}</h4>
-            <p><img height="200" width="200" src="${data.drinks[dIndex].strDrinkThumb}"></p>
-            ${ingredientsData}
-            ${instructionsData}
-            </div>`;
+        cocktail = data.drinks[dIndex];
+        // Getting all details of a drink using ID
+        d_id = cocktail.idDrink;
+        fetch(idUrl + d_id)
+          .then((res) => res.json())
+          .then((data) => displayData(data));
       }
     });
 }
+// This loop returns a random drink from an array of drinks along with the drink's ingredients paired with measurements, image, and instructions
+function displayData(data) {
+  console.log(data);
+  let drink = data.drinks[0];
+  let ingredients = [];
+  let measures = [];
+  let ingredientsData, instructionsData, ingred_measures;
+  for (let k = 1; k <= 15; k++) {
+    if (`strIngredient${k}` in drink && drink[`strIngredient${k}`] != null) {
+      ingredients.push(drink[`strIngredient${k}`]);
+    } else {
+      break;
+    }
+  }
 
+  for (let a = 1; a <= 15; a++) {
+    if (`strIngredient${a}` in drink && drink[`strMeasure${a}`] != null && drink[`strMeasure${a}`] != "") {
+      measures.push(drink[`strMeasure${a}`]);
+    } else {
+      break;
+    }
+  }
+  // console.log(ingredients);
+  ingred_measures = `<ul>`;
+  for (let b = 0; b < ingredients.length; b++) {
+    if (measures[b] == null || measures[b] == undefined) {
+      measures[b] = "";
+    }
+    ingred_measures += `<li>${ingredients[b]} - ${measures[b]}</li>`;
+  }
+  if (ingredients.length != 0) {
+    ingredientsData = `<p><b>Ingredients</b>: ${ingred_measures}</p>`;
+  } else {
+    ingredientsData = "";
+  }
+
+  ingred_measures += `</ul>`;
+  // if (measures.length != 0) {
+  //   ingred_measures = `<p><b>Measurements</b>: ${measures.join(", ")}</p>`;
+  // }else{
+  //   ingred_measures = '';
+  // }
+  if (drink.strInstructions !== undefined) {
+    instructionsData = `<p><b>Instructions</b>: ${drink.strInstructions}</p>`;
+  } else {
+    instructionsData = "";
+  }
+  drinkList.innerHTML += `<div>
+  <h4 style="font-size: 22px;">${drink.strDrink}</h4>
+  <p><img height="200" width="200" src="${drink.strDrinkThumb}"></p>
+  ${ingredientsData}
+  ${instructionsData}
+  </div>`;
+}
 // EVENT LISTENERS
 // init
 searchForm.addEventListener("submit", mainSearch);
