831e8bf1f72bd1b030f23d1096bcf1761cbfed9f
cadienlake <115520010+cadienlake@users.noreply.github.com>
2022-12-01 21:58:29 -0600

    Successfully calling and displaying measurements from Javascript.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -19,7 +19,7 @@ function mainSearch(event) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data.drinks);
+      // console.log(data.drinks);
       let indexes = [];
       //   RANDOM INFO
       //   This function returns x number of drinks from an array of drinks
@@ -39,26 +39,45 @@ function mainSearch(event) {
       // This loop returns a random drink from an array of drinks along with the drink's ingredients, image, and instructions
       for (let i = 0; i < indexes.length; i++) {
         let ingredients = [];
+        let measurements = [];
         dIndex = indexes[i];
-        let ingredientsData, instructionsData;
+        let ingredientsData = "";
+        let instructionsData = "";
         for (let k = 1; k <= 15; k++) {
           if (`strIngredient${k}` in data.drinks[dIndex] && data.drinks[dIndex][`strIngredient${k}`] != null) {
             ingredients.push(data.drinks[dIndex][`strIngredient${k}`]);
           } else {
             break;
           }
+          if (`strMeasure${k}` in data.drinks[dIndex] && data.drinks[dIndex][`strMeasure${k}`] != null) {
+            measurements.push(data.drinks[dIndex][`strMeasure${k}`]);
+          } else {
+            break;
+          }
         }
-        // console.log(ingredients);
+        console.log(measurements);
+        console.log(ingredients);
         if (ingredients.length != 0) {
-          ingredientsData = `<p>Ingredients: ${ingredients.join(", ")}</p>`;
-        } else {
-          ingredientsData = "";
+          ingredients.forEach((ingredient, index) => {
+            ingredientsData += `<li>${measurements[index]} ${ingredient}</li>`;
+          });
+          // ingredientsData = `<p>Ingredients: ${ingredients.join(", ")}</p>`;
         }
         if (data.drinks[dIndex].strInstructions !== undefined) {
-          instructionsData = `<p>Instructions: ${data.drinks[dIndex].strInstructions}</p>`;
+          instructionsData = `<h4 class="">Instructions:</h4><p class="text-sm">${data.drinks[dIndex].strInstructions}</p>`;
         } else {
           instructionsData = "";
         }
+        console.log(ingredientsData);
+        //     drinkList.innerHTML += `<div class="flex flex-col border-2 border-red-700 w-60 p-1 bg-red-100">
+        //     <img src="${data.drinks[dIndex].strDrinkThumb}" alt="">
+        //     <h3 class="text-center font-bold text-lg">${data.drinks[dIndex].strDrink}</h3>
+        //     <br>
+        //     ${ingredientsData}
+        //     ${instructionsData}
+        //     <button class="bg-red-300 border border-black p-1 justify-center">Favorite!</button>
+        // </div>`;
+
         drinkList.innerHTML += `<div>
             <h4 style="font-size: 22px;">${data.drinks[dIndex].strDrink}</h4>
             <p><img height="200" width="200" src="${data.drinks[dIndex].strDrinkThumb}"></p>
